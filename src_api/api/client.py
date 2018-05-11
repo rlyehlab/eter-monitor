f25e190c435fedd2ff90d6218b271211f5058889
vaap <vaap@disroot.org>
2018-05-10 21:23:50 -0300

    se comienza a utilizar paho-mqtt como client.

--- src_api/api/client.py
+++ src_api/api/client.py
@@ -1,31 +1,13 @@
-from flask_mqtt import Mqtt
+import paho.mqtt.client as mqtt
 
 
 
-mqtt = Mqtt()
+def on_connect(client, userdata, flags, rc):
+    print(“CONNACK received with code %d.” % (rc))
 
 
-#@mqtt.on_message()
-#def handle_mqtt_message(client, userdata, message):
-#    data = dict(
-#            topic=message.topic,
-#            payload=message.payload.decode()
-#            )
-#    print(data)
-#    topic = data['topic'].split('/')
-#    lugar = topic[0]
-#    sensor = topic[1]
-#    variable = topic[2]
-#    value = data['payload']
-#    if variable == 'TEMP':
-#        #print(value)
-#        med_db = Measure(sensor_id=sensor, device_id=1, value=value)
-#    elif variable == 'HUM':
-#        #print(value)
-#        med_db = Measure(sensor_id=sensor, device_id=2, value=value)
-#    db.session.add(med_db)
-#    db.session.commit()
-#
-#    return data
-#
 
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect(“localhost”, 1883)
